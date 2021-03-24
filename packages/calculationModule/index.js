cfd41309b468991fa86aa7141e44cf3d2c6e3da2
rajmohanpdy <raj4057@gmail.com>
2021-03-24 21:04:04 +0530

    first commit

---
+++ packages/calculationModule/index.js
@@ -0,0 +1,7 @@
+var add = require('additionModule');
+var subtract = require('subtractionModule');
+ 
+var sum = add.add(2,3);
+var diff = subtract.subtract(3,2);
+ 
+console.log("Sum: " + sum + " Diff: " + diff);
